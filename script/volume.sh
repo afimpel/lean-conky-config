4228272603d1eab89a50276e943c0bae387df46a
Alvaro Fimpel <afimpel@afimpel.com>
2022-03-24 01:19:57 -0300

    fix volumen

--- script/volume.sh
+++ script/volume.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 if hash pactl 2>/dev/null; then
-   name=$(pactl list sinks |grep -A 20 "RUNNING" | grep "alsa.name" | cut -d '"' -f2)
-   volu=$(pactl list sinks |grep -A 20 "RUNNING" | grep "Volume" | grep "front" -m 1 | cut -d '/' -f4 | cut -d '%' -f1)
-   echo "${name}:	${volu}%"
+   name=$(pactl list sinks |grep -A 20 "RUNNING" | grep -m1 "alsa.name" | cut -d '"' -f2)
+   volu=$(pactl list sinks |grep -A 20 "RUNNING" | grep "Volume" | grep "front" -m 1 | cut -d '/' -f4)
+   if [ ! -z "$name" ]
+   then
+      echo "\${alignc}${name}\${alignr}\${color2}${volu}"
+   else
+      echo '${alignr}Muted'
+   fi
 else
    volu=$(awk -F"[][]" '/dB/ { print $2 }' <(amixer sget Master))
-   echo "Master:	${volu}"
+   echo "\${alignc}Master:	\${alignr}\${color2}${volu}"
 fi
